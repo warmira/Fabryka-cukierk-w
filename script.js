ee1dfbe4ab4aeb5d8ce152a9f2c46d75c76b35c0
Patrycja Grzenkowicz <patrycja.grzenkowicz@onet.eu>
2024-05-12 08:28:46 +0200

    Arkusz E.14-03-20.01-SG - Styczeń 2020 - Wersja #03

---
+++ script.js
@@ -0,0 +1,22 @@
+function order() {
+    let shape = document.getElementById("shape").value;
+    let r = document.getElementById("r").value;
+    let g = document.getElementById("g").value;
+    let b = document.getElementById("b").value;
+    let napis = "Twoje zamówienie to cukierek ";
+    let color = "rgb(" + r + "," + g + "," + b + ")";
+    if (shape == 1) {
+        napis += "cytryna";
+    }
+    else if (shape == 2) {
+        napis += "liść";
+    }
+    else if ( shape == 3) {
+        napis += "banan";
+    }
+    else {
+        napis += "inny";
+    }
+    document.getElementById("score").innerHTML = napis;
+    document.getElementById("color").style.backgroundColor = color;
+}
