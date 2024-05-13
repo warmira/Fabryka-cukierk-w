9337158971dd582a0131757c68bc5a99826ffb2e
Patrycja Grzenkowicz <patrycja.grzenkowicz@onet.eu>
2024-05-13 13:30:56 +0200

    Poprawa działania pliku JS

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function order() {
     let b = document.getElementById("b").value;
     let napis = "Twoje zamówienie to cukierek ";
     let color = "rgb(" + r + "," + g + "," + b + ")";
-    if (shape == 1) {
+    if (shape == "1") {
         napis += "cytryna";
     }
-    else if (shape == 2) {
+    else if (shape == "2") {
         napis += "liść";
     }
-    else if ( shape == 3) {
+    else if ( shape == "3") {
         napis += "banan";
     }
     else {
